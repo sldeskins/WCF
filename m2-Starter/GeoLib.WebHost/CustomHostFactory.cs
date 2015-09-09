95aea2e66e9428b77f32e630dc626e15b824143f
sldeskins <sldeskins@gmail.com>
2015-09-09 17:47:03 -0400

    configuration of endpoints

---
+++ m2-Starter/GeoLib.WebHost/CustomHostFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+
+namespace GeoLib.WebHost
+{
+    public class CustomHostFactory : ServiceHostFactory
+    {
+        protected override ServiceHost CreateServiceHost ( Type serviceType, Uri[] baseAddresses )
+        {
+            ServiceHost host = new ServiceHost(serviceType, baseAddresses);
+
+            return host;
+        }
+
+    }
+
+}
