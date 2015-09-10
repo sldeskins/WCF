506da11644afe3fea262bcc902dbb76546c9de3b
sldeskins <sldeskins@gmail.com>
2015-09-10 18:43:43 -0400

    client proxy example

---
+++ m2-Starter/GeoLib.Proxies/GeoClient.cs
@@ -0,0 +1,38 @@
+﻿using GeoLib.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace GeoLib.Proxies
+{
+    public class GeoClient : ClientBase<IGeoService>, IGeoService
+    {
+        public GeoClient (string endpointName):base(endpointName)
+        {
+
+        }
+
+        public ZipCodeData GetZipInfo ( string zip )
+        {
+            return Channel.GetZipInfo(zip);
+        }
+
+        public IEnumerable<string> GetStates ( bool primaryOnly )
+        {
+            return Channel.GetStates(primaryOnly);
+        }
+
+        public IEnumerable<ZipCodeData> GetZips ( string state )
+        {
+            return Channel.GetZips(state);
+        }
+
+        public IEnumerable<ZipCodeData> GetZips ( string zip, int range )
+        {
+            return Channel.GetZips(zip, range);
+        }
+    }
+}
---
+++ m2-Starter/GeoLib.Tests/ManagerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using GeoLib.Data;
+using GeoLib.Services;
+using GeoLib.Contracts;
+
+namespace Geo.Tests
+{
+    [TestClass]
+    public class ManagerTests
+    {
+        [TestMethod]
+        public void test_zip_code_retrieval ()
+        {
+            Mock<IZipCodeRepository> mockZipCodeRepository = new Mock<IZipCodeRepository>();
+            ZipCode zipCode = new ZipCode()
+            {
+                City = "Lincoln Park",
+                State = new State() { Abbreviation = "NJ" },
+                Zip = "07035"
+            };
+
+            mockZipCodeRepository.Setup(obj => obj.GetByZip("07035")).Returns(zipCode);
+
+            //--
+            IGeoService geoService = new GeoManager(mockZipCodeRepository.Object);
+            ZipCodeData data = geoService.GetZipInfo("07035");
+
+            Assert.IsTrue(data.City == "Lincoln Park");
+            Assert.IsTrue(data.State == "NJ");
+        }
+    }
+}
